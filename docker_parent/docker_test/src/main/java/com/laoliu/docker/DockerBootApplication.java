65130149d0d8be1f215754dcd5fb41ceda9e310d
Xin <1498973064@qq.com>
2022-04-12 16:56:33 +0800

    version1

---
+++ docker_parent/docker_test/src/main/java/com/laoliu/docker/DockerBootApplication.java
@@ -0,0 +1,16 @@
+package com.laoliu.docker;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+
+@SpringBootApplication
+@MapperScan("com.laoliu.docker.mapper")
+public class DockerBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerBootApplication.class, args);
+    }
+}
---
+++ docker_parent/docker_test/src/main/java/com/laoliu/docker/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.laoliu.docker.mapper;
+
+import com.laoliu.docker.entities.User;
+import tk.mybatis.mapper.common.Mapper;
+
+
+public interface UserMapper extends Mapper<User>{
+
+
+}
